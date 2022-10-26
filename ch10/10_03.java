25d03da3e50d18ede829c554750e53d1f31ec238
yanyu100 <115055394+yanyu100@users.noreply.github.com>
2022-10-26 11:19:00 +0800

    Create 10_03.java

---
+++ ch10/10_03.java
@@ -0,0 +1,43 @@
+class Caaa
+{
+   public int num1;
+   public int num2;
+
+   public Caaa() //(a)
+   {
+     num1=1;
+     num2=1;
+   }
+   public Caaa(int a,int b)  //(b)
+   {
+      num1=a;
+      num2=b;
+   }
+   
+}
+
+class Cbbb extends Caaa
+{
+   public Cbbb()
+   {}
+   public Cbbb(int a,int b)  //(c)
+   {
+      super(a,b);
+   }
+   public void show()
+   {
+      System.out.println("num1="+num1+", num2="+num2);
+   }
+}
+
+public class hw10_3
+{
+   public static void main(String args[])
+   {
+      Cbbb b1=new Cbbb();
+      Cbbb b2=new Cbbb(3,9);
+
+      b1.show();
+      b2.show();
+   }
+}
