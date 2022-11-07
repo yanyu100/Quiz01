0fbf6e44c1e0a6e5afa993020033c3e6494535bb
yanyu100 <115055394+yanyu100@users.noreply.github.com>
2022-11-07 18:32:46 +0800

    Create w11_08.java

---
+++ ch11/w11_08.java
@@ -0,0 +1,32 @@
+interface iAaa
+{
+   public void show();
+}
+
+class Cbbb
+{
+   public int num=10;
+
+   public void set(int n)
+   {
+      num=n;
+   }
+}
+
+class Cccc extends Cbbb implements iAaa
+{
+   public void show()
+   {
+      System.out.println("num="+num);
+   }
+}
+
+public class w11_08
+{
+   public static void main(String args[])
+   {
+      Cccc cc=new Cccc();
+      cc.set(5);
+      cc.show();  
+   }
+}
