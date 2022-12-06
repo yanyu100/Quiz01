49db91ef168bad675a0e9a9ddf17fab4e4afa9e9
yanyu100 <115055394+yanyu100@users.noreply.github.com>
2022-12-06 22:49:38 +0800

    Create w12_14.java

---
+++ ch12/w12_14.java
@@ -0,0 +1,9 @@
+public class w12_14
+{
+   public static void main(String args[])
+   {
+     String str="Habit is second nature.";
+     System.out.println("string="+str);
+     System.out.println("sub-string="+str.substring(16,22));
+   }
+}
