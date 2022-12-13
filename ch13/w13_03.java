c7bc51dff1830835da93a1b0839d803d87760f28
yanyu100 <115055394+yanyu100@users.noreply.github.com>
2022-12-13 20:36:56 +0800

    Create w13_03.java

---
+++ ch13/w13_03.java
@@ -0,0 +1,20 @@
+public class w13_03
+{
+   public static void main(String args[])
+   {
+      try
+      {
+         int num=12,den=0;
+         int ans=num/den;
+         System.out.println("ans="+ans);
+      }
+      catch(ArithmeticException e)
+      {
+         System.out.println("°£¼Æ¬° 0");
+      }
+      finally
+      {
+         System.out.println("end of main() method!!");
+      }
+   }
+}
